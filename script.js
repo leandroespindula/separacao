065ca32b32a85fd0aff04f7c2db8b07edafe562f
Leandro <leandroepindula@gmail.com>
2023-05-01 11:01:27 -0300

    Adicionando logo

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let currentArrayIndex = 0; // declare currentArrayIndex as a global variable
+let csvArray;
+
+
 function parseCSV(text) {
   const rows = text.split('\n');
   const result = [];
@@ -10,17 +14,14 @@ function parseCSV(text) {
   return result;
 }
 
-
 function importarCSV(event) {
   const file = event.target.files[0];
   const reader = new FileReader();
   reader.readAsText(file);
   reader.onload = function() {
     const csvText = reader.result;
-    const csvArray = parseCSV(csvText);
+    csvArray = parseCSV(csvText);
     console.log(csvArray); // Exibe o array de arrays no console// Adicione este código dentro da função onload do FileReader
-
-    let currentArrayIndex = 0;
 
     const descricao = document.getElementById('descricao');
     const codigo = document.getElementById('codigo');
@@ -29,56 +30,56 @@ function importarCSV(event) {
     const caixa = document.getElementById('caixa');
     const quantidade = document.getElementById('quantidade');
 
+    // Update HTML with values from current CSV row
+    updateHTML();
+
     document.getElementById('verificar').addEventListener('click', function() {
-      descricao.textContent = csvArray[currentArrayIndex][2];
-      codigo.textContent = csvArray[currentArrayIndex][0];
-      saldo.textContent = `saldo picking: ${csvArray[currentArrayIndex][5]}`;
-      localizacao.textContent = csvArray[currentArrayIndex][4];
-      
-      const caixaStr = csvArray[currentArrayIndex][1];
-      console.log(caixaStr)
-      const lastNum = parseInt(caixaStr.substring(caixaStr.length - 2));
-      console.log(lastNum)
-      caixa.textContent = `CAIXA: ${lastNum}`;
-      
-      quantidade.textContent = `QTD: ${csvArray[currentArrayIndex][3]}`;
       currentArrayIndex++;
+      updateHTML();
     });
     
-    
-    
     document.getElementById('repor').addEventListener('click', function() {
-      // lógica do botão Repor
-      descricao.textContent = csvArray[currentArrayIndex][2];
-      codigo.textContent = csvArray[currentArrayIndex][0];
-      saldo.textContent = csvArray[currentArrayIndex][5];
-      localizacao.textContent = csvArray[currentArrayIndex][4];
-      caixa.textContent = `CAIXA: ${csvArray[currentArrayIndex][1]}`;
-      quantidade.textContent = csvArray[currentArrayIndex][3];
       currentArrayIndex++;
+      updateHTML();
     });
     
     document.getElementById('ok').addEventListener('click', function() {
-      // lógica do botão OK
-      descricao.textContent = csvArray[currentArrayIndex][2];
-      codigo.textContent = csvArray[currentArrayIndex][0];
-      saldo.textContent = csvArray[currentArrayIndex][5];
-      localizacao.textContent = csvArray[currentArrayIndex][4];
-      caixa.textContent = `CAIXA: ${csvArray[currentArrayIndex][1]}`;
-      quantidade.textContent = csvArray[currentArrayIndex][3];
       currentArrayIndex++;
+      updateHTML();
     });
     
     document.getElementById('faltou').addEventListener('click', function() {
-      // lógica do botão Faltou
-      descricao.textContent = csvArray[currentArrayIndex][2];
-      codigo.textContent = csvArray[currentArrayIndex][0];
-      saldo.textContent = csvArray[currentArrayIndex][5];
-      localizacao.textContent = csvArray[currentArrayIndex][4];
-      caixa.textContent = `CAIXA: ${csvArray[currentArrayIndex][1]}`;
-      quantidade.textContent = csvArray[currentArrayIndex][3];
       currentArrayIndex++;
+      updateHTML();
     });
-    
   }
+}
+
+// Add these two event listeners to your code
+document.getElementById('next-btn').addEventListener('click', function() {
+  if (currentArrayIndex < csvArray.length - 1) {
+    currentArrayIndex++;
+    updateHTML();
+  }
+});
+
+document.getElementById('prev-btn').addEventListener('click', function() {
+  if (currentArrayIndex > 0) {
+    currentArrayIndex--;
+    updateHTML();
+  }
+});
+
+// This function updates the HTML elements with the values from the current CSV row
+function updateHTML() {
+  descricao.textContent = csvArray[currentArrayIndex][2];
+  codigo.textContent = csvArray[currentArrayIndex][0];
+  saldo.textContent = `saldo picking: ${csvArray[currentArrayIndex][5]}`;
+  localizacao.textContent = csvArray[currentArrayIndex][4];
+  
+  const caixaStr = csvArray[currentArrayIndex][1];
+  const lastNum = parseInt(caixaStr.substring(caixaStr.length - 2));
+  caixa.textContent = `CAIXA: ${lastNum}`;
+  
+  quantidade.textContent = `QTD: ${csvArray[currentArrayIndex][3]}`;
 }
